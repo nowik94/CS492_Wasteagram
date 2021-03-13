e86cf0cb69d90a0929f8479880502ff989d000fa
Kiwon Nam <kiwonnam2016@u.northwestern.edu>
2021-03-13 01:40:14 -0600

    created scaffold with appbar and circularprogressindicator()

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:wasteagram/screens/list_screen.dart';
 
 class App extends StatelessWidget {
+
+  static final routes = {
+    ListScreen.routeName: (context) => ListScreen()
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wasteagram',
       theme: ThemeData(brightness: Brightness.dark),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Wasteagram')),
-      )
+      routes: routes,
     );
   }
 }
--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 // screen that will display all food waste posts by date
 
 class ListScreen extends StatelessWidget {
+  // route
+  static const routeName = '/';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text('Wasteagram')),
+      body: Center(
+        child: CircularProgressIndicator()
+      ),
+    );
   }
 }
