5f4468159a857b46b8d80757cdce3d6c54f0bd5f
Kiwon Nam <kiwonnam2016@u.northwestern.edu>
2021-03-13 17:48:56 -0600

    added initial streambuilder to list screen to obtain posts from firebase

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 // screen that will display all food waste posts by date
 
@@ -12,9 +14,26 @@ class ListScreen extends StatelessWidget {
       appBar: AppBar(
           centerTitle: true,
           title: Text('Wasteagram')),
-      body: Center(
-        child: CircularProgressIndicator()
-      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('food_waste_posts').snapshots(),
+        builder: (BuildContext content, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(snapshot.hasData && snapshot.data.docs != null && snapshot.data.docs.length > 0){
+            return ListView.builder(
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (context, index){
+                var post = snapshot.data.docs[index];
+                return ListTile(
+                  leading: Text(post['quantity'].toString()),
+                  title: Text('Post Title')
+                );
+              }
+            );
+        } else {
+          return Center(
+            child: CircularProgressIndicator());
+          }
+        }
+      )
     );
   }
 }
