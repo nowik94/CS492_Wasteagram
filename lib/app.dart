6119aa4e024d5dc553d3fe1815a087696f2dc841
Kiwon Nam <kiwonnam2016@u.northwestern.edu>
2021-03-13 23:09:53 -0600

    changed camerafab to stateful widget, added getImage() function to access gallery

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:wasteagram/screens/list_screen.dart';
-
+import 'package:wasteagram/screens/new_post_screen.dart';
 class App extends StatelessWidget {
 
   static final routes = {
-    ListScreen.routeName: (context) => ListScreen()
+    ListScreen.routeName: (context) => ListScreen(),
+    NewPostScreen.routeName: (context) => ListScreen()
   };
 
   @override
